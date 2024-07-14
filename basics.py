1c977bec1e04298b66dca8e007c237ca127db5e4
mariamsafa <mariamsafa153@gmail.com>
2024-07-14 11:08:29 +0600

    basics

---
+++ basics.py
@@ -0,0 +1,45 @@
+# import sys
+# print(sys.version)
+'''  multiple
+lines'''
+
+#creating variable by assigning values
+x=0 #integer
+y="i am safa" #string
+e,f,g=1,4.5,"bubt" #creating multiple variables in one line
+print(x , y)
+print(e,f,g)
+
+
+#intendation of block
+if 5>2:
+    print("intendation")
+
+if 6>0:
+          print("intendation2")
+
+
+#casting ( if we want to specify the data type)
+a= str('safa') # it also can be declared with double quotes "safa"
+b= int(59548)
+c= float(5.6)
+print(a,b,c)
+#we can print the data type of a variable by using type function()
+d=type(b)
+print(d) #<class 'int'>
+
+
+
+#list
+a=2
+b=3
+animals = ["tiger",'monkey',"lion"]
+x,y,z = animals
+print(x,y,z)
+print(x + y +z)
+print(a+b)
+
+
+
+
+
