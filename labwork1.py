101323b5f9d53bfe8b01f2ba98cd5c59399ff007
Afia Parvin Maria <88893709+mariamsafa@users.noreply.github.com>
2024-08-25 18:16:15 +0600

    Create labwork1.py

---
+++ labwork1.py
@@ -0,0 +1,31 @@
+#1.convert the variable x from an integer to a float
+x = 5
+cast= float(x)
+print(cast)
+
+#2
+a="mariam"
+b="safa"
+x= a+b #concatenated
+print(x)
+
+#3
+mylist =["USA","123","mariam","safa"]
+print(mylist[2])
+
+#4
+set1 = {"jackfruit", "mango", "cherry"}
+set2={"Lion","tiger","bird"}
+set3= set1|(set2)
+print(set3)
+
+#5
+my_dict = {
+  "myname": "safa",
+  "age": "24",
+  "year": 2000
+}
+my_dict["address"] = "dhaka"
+print(my_dict)
+
+#6
