09328e8d78d9cc3cd686ba99374718c383ae351e
mariamsafa <mariamsafa153@gmail.com>
2024-09-22 17:30:13 +0600

    changed datetime file to date_time

--- pandas_example.py
+++ pandas_example.py
@@ -1,12 +1,12 @@
-import pandas as safa
-
-
+import pandas as pd
 
 mydataset = {
   'cars': ["BMW", "Volvo", "Ford"],
   'passings': [3, 7, 2]
 }
-
-myvar = safa.DataFrame(mydataset)
+s=[1,2,3,4]
+myvar= pd.Series(s)
+print(myvar)
+myvar = pd.DataFrame(mydataset)
 
 print(myvar)
